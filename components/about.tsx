9a6ac4e9e448931817f4b2def585dbc6fc60d20f
Kolby Kiernan <kolby.kiernan@gethealthie.com>
2025-01-30 16:07:15 -0800

    Fixing deployed error

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import React from 'react';
-import SectionHeading from './section-heading';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
+import SectionHeading from './section-heading';
 
 
 export default function About() {
